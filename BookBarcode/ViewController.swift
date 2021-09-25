d903362814be047d874c87ba51a5548897e2dfb3
eijis77 <71688271+eijis77@users.noreply.github.com>
2021-09-25 20:50:10 +0900

    Initial Commit

---
+++ BookBarcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookBarcode
+//
+//  Created by Eiji Shiba on 2021/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
